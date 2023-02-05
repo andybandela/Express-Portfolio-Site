465a8c0b0831f87c0d8221abf1e7085a11665532
Andy Bandela <andybandela@Andys-MBP.phub.net.cable.rogers.com>
2023-02-05 00:12:39 -0500

    finish contact page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+//Redirecting users to the home page after subimitting the form on the contact page
+router.post('/',(req,res,next)=>{
+  res.redirect('/');
+});
 
 module.exports = router;
