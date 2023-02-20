abca0f82540115226d2b0ad9aa9ae074f508b6d6
Andy Bandela <andybandela@Andys-MBP.phub.net.cable.rogers.com>
2023-02-19 23:37:56 -0500

    uri connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 //db.js Andy Bandela 301282674 17/02/2023
 module.exports = {
     url : "mongodb://127.0.0.1:27017/userDB",
-    uri:"mongodb+srv://andybandela:<password>@cluster0.noh3rlk.mongodb.net/?retryWrites=true&w=majority",
+    uri:"mongodb+srv://andybandela:M3PrrmyTOekZMDaB@cluster0.noh3rlk.mongodb.net/?retryWrites=true&w=majority",
     option : {
         useNewUrlParser: true,
         useUnifiedTopology: true,
